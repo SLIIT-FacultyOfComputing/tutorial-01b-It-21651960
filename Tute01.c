97c479c664281f0e32f3d991813c13167463d0dd
It216519601 <87486287+It-21651960@users.noreply.github.com>
2021-07-15 15:59:11 +0000

    tute 1

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,15 @@
 #include <stdio.h>
 
 int main() {
+
+int x,y;
+float avg;//marks
+
+printf ("Enter marks  :");//printing 
+scanf ("%d%d",&x,&y);//input marks
+
+avg= (x+y)/2;
+printf("Average :%.2f",avg);
   
   return 0;
 }
